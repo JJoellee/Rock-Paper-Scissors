3961b6c51dcbec88124f07b02a87def6355fe24a
JJoellee <88345049+JJoellee@users.noreply.github.com>
2022-12-22 17:31:34 +0200

    Add images and style

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+//global vars
 let playerScore=0;
 let computerScore=0;
 let winner;
 
+//elements
+let result = document.querySelector('#result'); //result for one round
+let statement = document.querySelector('#statement'); //statement of one round
+let startContainer = document.getElementById("start-container");
+let start = document.querySelector('#start');
+let gameContainer = document.getElementById("game-container");
+let buttons = document.querySelectorAll('.gameBtn');
+let endContainer = document.getElementById("end-container");
+let end = document.querySelector('#end'); //end result
+
+
+//get computer choice
 function getComputerChoice(){
     let choice;
     const number = Math.floor(Math.random()*3);
@@ -11,35 +24,55 @@ function getComputerChoice(){
     return choice;
 }
 
+// play one round
 function playRound(playerSelection, computerSelection){
-    let result = document.querySelector('#result');
-    let end = document.querySelector('#end');
     playerSelection = playerSelection.toLowerCase();
-    if(playerSelection==computerSelection){result.textContent = "You "+ playerScore + " - " + computerScore+" Bot"+ " \nTie.";} 
+    if(playerSelection==computerSelection){statement.textContent="Tie.";} 
     else if((playerSelection=="rock" && computerSelection=="paper") 
     || (playerSelection=="paper" && computerSelection=="scissors") 
     || (playerSelection=="scissors" && computerSelection=="rock")){
         computerScore++;
-        result.textContent = "You "+ playerScore + " - " + computerScore+" Bot"+ " \nYou lose :( "+ computerSelection + " beats " + playerSelection;
+        statement.textContent= "You lose :( Computer chose "+ computerSelection + " beating your " + playerSelection;
     } 
     else {
         playerScore++;
-        result.textContent = "You "+ playerScore + " - " + computerScore+" Bot"+ " \nYou win!! " +  playerSelection + " beats " + computerSelection;
+        statement.textContent="You win!! You chose " +  playerSelection + " beating the computer's " + computerSelection;
     }
+    result.textContent = "You "+ playerScore + " - " + computerScore+ " Bot";
     if (playerScore==5 || computerScore==5){
         if(playerScore>computerScore) winner="You";
         else winner= "Bot";
         end.textContent= winner + " won!";
+        hideGame_showEnd();
     }
 
 }
 
-let buttons = document.querySelectorAll('button');
+//add event listeners to buttons
 buttons.forEach(button => {
     button.addEventListener("click", ()=>{
-        playRound(button.textContent, getComputerChoice());
+        playRound(button.id, getComputerChoice());
     }
     );
 }
 )
+
+// hide start div and show game div
+start.addEventListener('click', function(){
+    playerScore=0; computerScore=0; winner="";
+    result.textContent = "You "+ playerScore + " - " + computerScore+" Bot";
+    startContainer.style.display='none';
+    gameContainer.style.display= 'block';
+})
+
+//hide game and show end, called when battle ends
+function hideGame_showEnd() {
+    gameContainer.style.display= 'none';
+    endContainer.style.cssText="display: block; text-align: center; "; 
+    let again = document.getElementById('again');
+    again.addEventListener('click', ()=>{
+        endContainer.style.display= 'none';
+        startContainer.style.display='block';
+    })
+}
 
